4ae46babc5270cd97f835693a0dae97d4882187e
Surya-palanisamy <suryasitit@gmail.com>
2024-11-29 22:54:12 +0530

    first commit

---
+++ client/src/SEO/SeoData.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet";
+
+// eslint-disable-next-line react/prop-types
+const SeoData = ({
+  title = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!",
+  description = "Flipkart online shopping platform clone by Aashish Dhiman",
+  keywords = ["shopping", "react project", "mern project", "nodejs", "online"],
+}) => (
+  <Helmet>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta name="keywords" content={keywords} />
+  </Helmet>
+);
+
+export default SeoData;
