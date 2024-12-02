9ad859e420afbd2f18cd740226e77f0b0873c672
Surya-palanisamy <suryasitit@gmail.com>
2024-12-02 20:19:55 +0530

    final

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,10 +14,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 //local imports
-import connectDB from "./config/database.js";
-import authRoute from "./routes/authRoute.js";
-import productRoute from "./routes/productRoute.js";
-import userRoute from "./routes/userRoute.js";
+import connectDB from "../config/database.jss";
+import authRoute from "../routes/authRoute.jss";
+import productRoute from "../routes/productRoute.jss";
+import userRoute from "../routes/userRoute.jss";
 
 //rest object
 const app = express();
